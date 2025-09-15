626db0175bdeb22270b30c53913f738c7514b423
DomiCarr <carrasco.dominique@gmail.com>
2025-09-15 21:16:07 +0200

    :construction: work in progress : exo07

--- exo07/main.py
+++ exo07/main.py
@@ -1,1 +1,13 @@
-## Écrivez votre code ici !
+# Écrivez votre code ici !
+def square(val):
+    try:
+        val = float(val)
+        print(f"val: {val}")
+        return val * val
+    except ValueError:
+        print("Le paramètre doit être un nombre !")
+        return None
+
+
+val = input("Donnez un nombre : ")
+print(f"val: {val} carré: {square(val)}")
