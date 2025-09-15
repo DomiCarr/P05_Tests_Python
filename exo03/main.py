efa5e165e46a3fe438e80700087c632771e8f844
DomiCarr <carrasco.dominique@gmail.com>
2025-09-15 20:35:10 +0200

    :construction: work in progress : exo03

--- exo03/main.py
+++ exo03/main.py
@@ -1,1 +1,8 @@
 words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+liste_words = []
+
+for word in words:
+    tup = (word, len(word))
+    liste_words.append(tup)
+
+print(liste_words)
