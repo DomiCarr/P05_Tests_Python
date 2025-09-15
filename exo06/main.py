702e7690a8627dcd281a0d9737fba844693f03eb
DomiCarr <carrasco.dominique@gmail.com>
2025-09-15 20:46:45 +0200

    :construction: work in progress : exo06

--- exo06/main.py
+++ exo06/main.py
@@ -1,6 +1,22 @@
 # Fonction calculate_average
- 
+
 # Exemple d'utilisation de la fonction
 numbers = [10, 20, 30, 40, 50]
 average = calculate_average(numbers)
 print("La moyenne est :", average)
+
+
+def calculate_average(numbers)
+    tot = 0
+    num = 0
+    for n in numbers:
+        tot = tot + n
+        num = num + 1
+    moy = tot/num
+    print(f"tot: {tot}")
+    print(f"moy: {moy}" )
+    return moy
+
+
+
+
